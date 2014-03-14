ec1b675d031e67dfdcc22ddcbb4bddfaed48aa97
William Oliveira <sudowilliam@gmail.com>
2014-03-14 14:35:25 -0300

    exercise 2 files

---
+++ Exercise2C/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 14, 2014
+ *  Author: William Oliveira <sudowilliam@gmail.com>
+ *
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	char * s = "Whats going on?\n";
+	printf(s);
+	return 0;
+}
+
+
+
